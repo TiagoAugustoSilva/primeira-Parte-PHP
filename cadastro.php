1ad5c4f14e1f5a764522dce0d4ff6b0485f3783f
tiagoaugusto <112525927+TiagoAugustoSilva@users.noreply.github.com>
2023-12-08 10:00:10 -0300

    Add files via upload
    Prmeira parte do crud Pronta

---
+++ cadastro.php
@@ -0,0 +1,110 @@
+<?php
+require_once("conexao.php");
+
+$id = 0;
+$produto = '';
+$descricao = '';
+$valor_unitario = '';
+$unidade_medida = '';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $id = isset($_POST['id']) ? filter_input(INPUT_POST, "id", FILTER_SANITIZE_NUMBER_INT) : null;
+    $produto = filter_input(INPUT_POST, "produto", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $descricao = filter_input(INPUT_POST, "descricao", FILTER_SANITIZE_NUMBER_INT);
+    $valor_unitario = filter_input(INPUT_POST, "valor_unitario", FILTER_SANITIZE_NUMBER_FLOAT);
+    $unidade_medida = filter_input(INPUT_POST, "unidade_medida", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    if (!$id) {
+        // Se $id não existe, prepara a instrução INSERT
+        $stm = $conn->prepare("INSERT INTO cadastrar_produto (produto, descricao, valor_unitario, unidade_medida) VALUES (:produto, :descricao, :valor_unitario, :unidade_medida)");
+    } else {
+        // Se $id existe, prepara a instrução UPDATE
+        $stm = $conn->prepare("UPDATE cadastrar_produto SET produto=:produto, descricao=:descricao, valor_unitario=:valor_unitario, unidade_medida=:unidade_medida WHERE id=:id");
+        $stm->bindValue(':id', $id);
+    }
+
+    // Adicione as linhas de vinculação abaixo para garantir que todos os parâmetros sejam vinculados corretamente
+    $stm->bindValue(':produto', $produto);
+    $stm->bindValue(':descricao', $descricao);
+    $stm->bindValue(':valor_unitario', $valor_unitario);
+    $stm->bindValue(':unidade_medida', $unidade_medida);
+
+    $stm->execute();
+
+    header('Location: index.php');
+    exit;
+}
+
+if (isset($_GET['id'])) {
+    $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+
+    if (!$id) {
+        header('Location: index.php');
+        exit;
+    }
+
+    $stm = $conn->prepare('SELECT * FROM cadastrar_produto WHERE id=:id');
+    $stm->bindValue(':id', $id);
+    $stm->execute();
+    $result = $stm->fetch();
+
+    if (!$result) {
+        header('Location: index.php');
+        exit;
+    }
+
+    $id = $result['id'];
+    $produto = $result['produto'];
+    $descricao = $result['descricao'];
+    $valor_unitario = $result['valor_unitario'];
+    $unidade_medida = $result['unidade_medida'];
+}
+
+include_once("./layout/_header.php");
+?>
+<div class="card mt-4">
+    <div class="card-header d-flex justify-content-between align-items-center">
+        <h5><?= $id ? 'codigo ' . $id .  ' Editar Produto ' : 'Adicionar Produto' ?></h5>
+        <?php if ($id) : ?>
+            <a class="btn btn-warning" href="cadastro.php?id=<?= $id ?>">Editar</a>
+        <?php endif; ?>
+    </div>
+
+    <form method="post" autocomplete="off">
+        <div class="card-body">
+            <input type="hidden" name="id" value="<?= $id ?>" />
+
+            <div class="form-group">
+                <label for="produto">Produto</label>
+                <input type="text" class="form-control" id="produto" name="produto" value="<?= $produto ?>" required />
+            </div>
+            <div class="form-group">
+                <label for="descricao">Descrição</label>
+                <select class="form-select" id="descricao" name="descricao" required>
+                    <option value="1" <?= $descricao == 1 ? 'selected' : '' ?>>Eletrônicos</option>
+                    <option value="2" <?= $descricao == 2 ? 'selected' : '' ?>>Informática</option>
+                    <option value="3" <?= $descricao == 3 ? 'selected' : '' ?>>Domésticos</option>
+                    <option value="4" <?= $descricao == 4 ? 'selected' : '' ?>>Celulares</option>
+                    <option value="5" <?= $descricao == 5 ? 'selected' : '' ?>>Acessórios</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="valor_unitario">Valor Unitário</label>
+                <input type="text" class="form-control" id="valor_unitario" name="valor_unitario" value="<?= $valor_unitario ?>" required />
+            </div>
+            <div class="form-group">
+                <label for="unidade_medida">Unidade de Medida</label>
+                <input type="text" class="form-control" id="unidade_medida" name="unidade_medida" value="<?= $unidade_medida ?>" required />
+            </div>
+        </div>
+
+        <div class="card-footer">
+            <button type="submit" class="btn btn-success">Salvar</button>
+            <a class="btn btn-primary" href="index.php">Voltar</a>
+        </div>
+    </form>
+</div>
+
+<?php
+include_once("./layout/_footer.php");
+?>
---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+
+require_once("conexao.php");
+
+//atenção este get está associado ao botão excluir, junto ao get global do código.
+if (isset($_GET['excluir'])) {
+    $id = filter_input(INPUT_GET, 'excluir', FILTER_SANITIZE_NUMBER_INT);
+
+    if($id);
+    $conn->exec('DELETE FROM cadastrar_produto WHERE id=' . $id);
+
+    header('Location: index.php');
+    exit;
+  
+}
+
+$results = $conn->query("select * from cadastrar_produto")->fetchAll();
+
+$arrayDescricao = [1 => 'Eletrônicos', 2 => 'Informática', 3 => 'Domésticos', 4 => 'Celulares', 5 => 'Acessórios'];
+
+include_once("./layout/_header.php");
+
+
+?>
+
+<?php
+
+require_once("conexao.php");
+
+include_once("./layout/_header.php");
+
+?>
+
+<!-- O mt-4 é o mesmo que margin-top: 4; aplicado ao elemento card -->
+
+<div class="card mt-4">
+    <!--justify-content-between joga o botão la no fim Chick D+-->
+    <div class="card-header d-flex justify-content-between align-items-center custom-card-header bg-warning">
+        <!-- o text-center my-4 mx-auto é o mesmo que display flex, justify-content-center align-items-center-->
+        <h5 class="text-center my-4 h5-striped ">Mini Faturamento</h5>
+        <!--ao utilizar btn-success ele dará a  cor verde ao botão pelo boostrap-->
+        <a class="btn btn-success" href="cadastro.php">+Adicionar</a>
+    </div>
+    <!-- Conteúdo do corpo do cartão -->
+    <div class="card-body">
+        <!--com bootstrap só de por a class="table" ele já ajusta a tabela-->
+        <table class="table table-striped table-dark">
+            <thead>
+                <tr>
+                    <th>Código </th>
+                    <th>Produto</th>
+                    <th>Descrição</th>
+                    <th>Valor Unitário</th>
+                    <th>Unidade de Medida</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($results as $item) : ?>
+                    <tr>
+                        <td><?= $item['id'] ?></td>
+                        <td><?= $item['produto'] ?></td>
+                        <td><?= $arrayDescricao[$item['descricao']] ?></td>
+                        <td><?= $item['valor_unitario'] ?></td>
+                        <td><?= $item['unidade_medida'] ?></td>
+                        <!--área abaixo onde colocamos os nosso btn(Buttons) lembrando que btn-primary botão azul e btn-danger-botão vermelho-->
+                        <td>
+                            <a class="btn btn-sm btn-primary" href="cadastro.php?id=<?= $item['id'] ?>">Editar</a>
+                            <button class="btn btn-sm btn-danger" onclick="excluir(<?=$item['id']?>)">Excluir</button>
+                        </td>
+
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+    </div>
+</div>
+
+<script>   
+ function excluir(id) {
+    if (confirm("Deseja Excluir este cadastro?")) {
+        window.location.href = "index.php?excluir=" + id;
+    }
+ }
+
+</script>
+
+<?php
+include_once("./layout/_footer.php");
+?>
