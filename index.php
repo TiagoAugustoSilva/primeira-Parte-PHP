862f8042f5053055e65bcb857b3ed264afeb1af7
tiagoaugusto <112525927+TiagoAugustoSilva@users.noreply.github.com>
2023-12-12 12:30:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 
 require_once("conexao.php");
 
-//atenção este get está associado ao botão excluir, junto ao get global do código.
+// Atenção: Este get está associado ao botão excluir, junto ao get global do código.
 if (isset($_GET['excluir'])) {
     $id = filter_input(INPUT_GET, 'excluir', FILTER_SANITIZE_NUMBER_INT);
 
-    if($id);
-    $conn->exec('DELETE FROM cadastrar_produto WHERE id=' . $id);
+    if ($id) {
+        $conn->exec('DELETE FROM cadastrar_produto WHERE id=' . $id);
 
-    header('Location: index.php');
-    exit;
-  
+        header('Location: index.php');
+        exit;
+    }
 }
 
 $results = $conn->query("select * from cadastrar_produto")->fetchAll();
@@ -20,72 +20,89 @@ $arrayDescricao = [1 => 'Eletrônicos', 2 => 'Informática', 3 => 'Domésticos',
 
 include_once("./layout/_header.php");
 
-
 ?>
 
-<?php
+<div class="container-fluid">
+    <div class="row">
+        <!-- Menu Lateral -->
+        <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block sidebar bg-dark mt-4">
+            <div class="position-sticky">
+                <ul class="nav flex-column">
+                <li class="nav-item">
+                            <h3 class="text-light">Menu</h3>
+                        </li>
+                    <li class="nav-item ">
+                        <a class="nav-link active" href="movimentacoes.php">
+                          <h5 class="text-light"> Controle de Estoque</h5> 
+                        </a>
+                    </li>
+                  
+                    <li class="nav-item ">
+                        <a class="nav-link " href="vendas.php">
+                            <h5 class="text-light">Venda Produtos</h5>
+                            
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
-require_once("conexao.php");
-
-include_once("./layout/_header.php");
-
-?>
-
-<!-- O mt-4 é o mesmo que margin-top: 4; aplicado ao elemento card -->
-
-<div class="card mt-4">
-    <!--justify-content-between joga o botão la no fim Chick D+-->
-    <div class="card-header d-flex justify-content-between align-items-center custom-card-header bg-warning">
-        <!-- o text-center my-4 mx-auto é o mesmo que display flex, justify-content-center align-items-center-->
-        <h2 class="text-center my-4 h2-striped">Mini Faturamento
-        <a class="btn btn-primary" href="cadastro.php">+Adicionar</a>
-        </h2>
-        <!--ao utilizar btn-success ele dará a  cor verde ao botão pelo boostrap-->
-        <a class="btn btn-dark" href="movimentacoes.php">Controle de Estoque</a>
-    </div>
-    <!-- Conteúdo do corpo do cartão -->
-    <div class="card-body">
-        <!--com bootstrap só de por a class="table" ele já ajusta a tabela-->
-        <table class="table table-striped table-dark">
-            <thead>
-                <tr>
-                    <th>Código </th>
-                    <th>Produto</th>
-                    <th>Descrição</th>
-                    <th>Valor Unitário</th>
-                    <th>Unidade de Medida</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($results as $item) : ?>
-                    <tr>
-                        <td><?= $item['id'] ?></td>
-                        <td><?= $item['produto'] ?></td>
-                        <td><?= $arrayDescricao[$item['descricao']] ?></td>
-                        <td><?= $item['valor_unitario'] ?></td>
-                        <td><?= $item['unidade_medida'] ?></td>
-                        <!--área abaixo onde colocamos os nosso btn(Buttons) lembrando que btn-primary botão azul e btn-danger-botão vermelho-->
-                        <td>
-                            <a class="btn btn-sm btn-primary" href="cadastro.php?id=<?= $item['id'] ?>">Editar</a>
-                            <button class="btn btn-sm btn-danger" onclick="excluir(<?=$item['id']?>)">Excluir</button>
-                        </td>
-
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-
+        <!-- Conteúdo Principal -->
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <div class="card mt-4">
+                <div class="card-header d-flex justify-content-between align-items-center custom-card-header bg-warning">
+                    <h2 class="text-center my-4 h2-striped">Cadastro Produto
+                        <a class="btn btn-primary" href="cadastro.php">+Adicionar</a>
+                    </h2>
+                    <a class="btn btn-dark" href="movimentacoes.php">Controle de Estoque</a>
+                </div>
+                <div class="card-body">
+                    <!-- Tabela de Cadastro de Produtos -->
+                    <table class="table table-striped table-dark">
+                        <!-- Cabeçalho da Tabela -->
+                        <thead>
+                            <tr>
+                                <th>Código</th>
+                                <th>Produto</th>
+                                <th>Quantidade</th>
+                                <th>Descrição</th>
+                                <th>Valor Unitário</th>
+                                <th>Unidade de Medida</th>
+                                
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <!-- Corpo da Tabela -->
+                        <tbody>
+                            <?php foreach ($results as $item) : ?>
+                                <tr>
+                                    <td><?= $item['id'] ?></td>
+                                    <td><?= $item['produto'] ?></td>
+                                    <td><?= $item['quantidade'] ?></td>
+                                    <td><?= $arrayDescricao[$item['descricao']] ?></td>
+                                    <td><?= $item['valor_unitario'] ?></td>
+                                    <td><?= $item['unidade_medida'] ?></td>
+                                    
+                                    <td>
+                                        <a class="btn btn-sm btn-primary" href="cadastro.php?id=<?= $item['id'] ?>">Editar</a>
+                                        <button class="btn btn-sm btn-danger" onclick="excluir(<?= $item['id'] ?>)">Excluir</button>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </main>
     </div>
 </div>
 
-<script>   
- function excluir(id) {
-    if (confirm("Deseja Excluir este cadastro?")) {
-        window.location.href = "index.php?excluir=" + id;
+<script>
+    function excluir(id) {
+        if (confirm("Deseja Excluir este cadastro?")) {
+            window.location.href = "index.php?excluir=" + id;
+        }
     }
- }
-
 </script>
 
 <?php
